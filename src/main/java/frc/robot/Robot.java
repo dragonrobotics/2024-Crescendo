776a842d5507b938e9510af7e855f03860009d58
Boomrbam <98230073+Boomrbam@users.noreply.github.com>
2024-02-28 16:21:05 -0600

    Elevator controls (#4)
    * Can't break itself extending and rotating. Shoot into speaker. values still need to be found.
    
    * Climb
    
    ---------
    
    Co-authored-by: Mattie Nash <42892531+OmegaMetor@users.noreply.github.com>

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -46,7 +46,7 @@ public class Robot extends TimedRobot {
      * LT: Automated Climb
      * (Elevator controls to be automated once reasonable)
      */
-/* 
+
     controller.leftBumper().and(() -> {
       return !(yeeter.HasNote() || intake.HasNote());
     }).whileTrue(intake.intakeNote());
@@ -80,21 +80,38 @@ public class Robot extends TimedRobot {
       return intake.HasNote() || yeeter.HasNote();
     }).whileTrue(
         either(run(() -> {yeeter.SetVoltage(4);}, yeeter)
-        .until(() -> {return yeeter.getSpeed() > 5; /*TODO: test for a better value *//*})
+        .until(() -> {return yeeter.getSpeed() > 5; /*TODO: test for a better value */})
         .andThen(run(() -> {intake.setVoltage(4);}, intake))
         .until(() -> {return !yeeter.HasNote() && !intake.HasNote();})
         .andThen(run(() -> {yeeter.Stop(); intake.stopIntake();}, intake, yeeter)), 
-        }, yeeter).until(() -> {
-          return !yeeter.HasNote();
-        }).andThen(waitSeconds(.2)).andThen(runOnce(() -> {
-          yeeter.Stop(); // TODO: Make arm go down if not doing trap.
-        })),
+        
+        run(() -> {
+          yeeter.SetVoltage(4 /* TODO: Test for a better value*/);
+        }, yeeter)
+        .until(() -> {return !yeeter.HasNote();})
+        .andThen(waitSeconds(.2))
+        .andThen(runOnce(() -> {yeeter.Stop(); }))
+        .andThen(runOnce(() -> {arm.SetAngle(0);})),
         
         () -> {
           return intake.HasNote();
         }));
 
- */    // TODO: Climbing controls
+      controller.povUp().and(() -> {return arm.getAngle() > 88;}).whileTrue(
+        run(() -> {arm.setExtension(5);})
+      );
+
+      controller.povDown().and(() -> {return arm.getAngle() > 88;}).whileTrue(
+        run(() -> {arm.setExtension(0);})
+      );
+
+      controller.povLeft().and(() -> {return arm.getExtension() < 1;}).whileTrue(
+        run(() -> {arm.setAngle(90);})
+      );
+
+      controller.povRight().and(() -> {return arm.getExtension() < 1;}).whileTrue(
+        run(() -> {arm.setAngle(0);})
+      );
   }
 
   @Override
